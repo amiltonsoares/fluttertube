9189b88354f55decb9203ab7de07835364c1e7d7
Amilton Soares <amilton@inovesistemas.com.br>
2020-01-26 19:20:35 -0300

    Versao Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/favorite_bloc.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
+import 'package:fluttertube/screens/home.dart';
+import 'api.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Api api = Api();
+  //api.search('eletro');
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      bloc: VideosBloc(),
+      child: BlocProvider(
+        bloc: FavoriteBloc(),
+        child: MaterialApp(
+          title: 'FlutterTube',
+          theme: ThemeData(
+            backgroundColor: Colors.black87,
+          ),
+          home: Home(),
+        ),
+      ),
+    );
   }
 }
